b5828a85f3cd82a43052cef25ddbb4a9fde200c8
VadimKovalenko <vadim.kovalenko@orangesoft.co>
2021-08-04 12:43:24 +0300

    root vc

--- MovieWorld/MovieWorld/ApplicationLayer/AppDelegate.swift
+++ MovieWorld/MovieWorld/ApplicationLayer/AppDelegate.swift
@@ -6,7 +6,12 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        window = UIWindow(frame: UIScreen.main.bounds)
+                let rootVC = BaseViewController()
+                
+                let rootNC = UINavigationController(rootViewController: rootVC)
+                window?.rootViewController = rootNC
+                window?.makeKeyAndVisible()
         return true
     }
 }
