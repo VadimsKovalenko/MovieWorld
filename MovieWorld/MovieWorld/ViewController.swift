a1120cc913d2e0e2cbd8fb007c5a96fa49c7a46d
VadimKovalenko <vadim.kovalenko@orangesoft.co>
2021-08-04 11:23:34 +0300

    create project

---
+++ MovieWorld/MovieWorld/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MovieWorld
+//
+//  Created by orangesoft on 4.08.21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
