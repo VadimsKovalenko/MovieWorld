f2d01513e571275e9a091b69d344cbdd3a2b689a
VadimKovalenko <vadim.kovalenko@orangesoft.co>
2021-08-05 11:28:54 +0300

    switch vc

--- MovieWorld/MovieWorld/ApplicationLayer/AppDelegate.swift
+++ MovieWorld/MovieWorld/ApplicationLayer/AppDelegate.swift
@@ -5,12 +5,14 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
 
-    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+    func application(
+        _ application: UIApplication,
+        didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         window = UIWindow(frame: UIScreen.main.bounds)
-                let rootVC = TabBarController()
-
-                window?.rootViewController = rootVC
-                window?.makeKeyAndVisible()
+        let rootVC = TabBarController()
+        
+        window?.rootViewController = rootVC
+        window?.makeKeyAndVisible()
         return true
     }
 }
--- MovieWorld/MovieWorld/PresentationLayer/TabBar/TabBarController.swift
+++ MovieWorld/MovieWorld/PresentationLayer/TabBar/TabBarController.swift
@@ -2,44 +2,44 @@ import UIKit
 
 final class TabBarController: UITabBarController {
     
-    // MARK: lets/vars
+    // MARK: Properties
     
     private lazy var mainVC: UINavigationController = {
         let vc = MainViewController()
         let nv = UINavigationController(rootViewController: vc)
         nv.tabBarItem.title = "Main"
         nv.tabBarItem.image = UIImage(named: "icon-tabbar-main")
-       return nv
+        return nv
     }()
     
     private lazy var categoryVC: UINavigationController = {
-        let vc = MainViewController()
+        let vc = CategoryViewController()
         let nv = UINavigationController(rootViewController: vc)
         nv.tabBarItem.title = "Category"
         nv.tabBarItem.image = UIImage(named: "icon-tabbar-category")
-       return nv
+        return nv
     }()
     
     private lazy var searchVC: UINavigationController = {
-        let vc = MainViewController()
+        let vc = SearchViewController()
         let nv = UINavigationController(rootViewController: vc)
         nv.tabBarItem.title = "Search"
         nv.tabBarItem.image = UIImage(named: "icon-tabbar-search")
-       return nv
+        return nv
     }()
     
     private lazy var profileVC: UINavigationController = {
-        let vc = MainViewController()
+        let vc = ProfileViewController()
         let nv = UINavigationController(rootViewController: vc)
         nv.tabBarItem.title = "Profile"
         nv.tabBarItem.image = UIImage(named: "icon-tabbar-profile")
-       return nv
+        return nv
     }()
-
+    
     // MARK: Lifecycle functions
     
     override func viewDidLoad() {
         super.viewDidLoad()
-       viewControllers = [mainVC,categoryVC,searchVC,profileVC]
+        viewControllers = [mainVC,categoryVC,searchVC,profileVC]
     }
 }
