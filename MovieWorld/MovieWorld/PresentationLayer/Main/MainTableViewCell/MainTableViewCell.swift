b88c2fe69c1abe3e8f9b18c9df828f5dcfa7dc14
VadimKovalenko <vadim.kovalenko@orangesoft.co>
2021-08-05 13:07:50 +0300

    add tableViewCell

---
+++ MovieWorld/MovieWorld/PresentationLayer/Main/MainTableViewCell/MainTableViewCell.swift
@@ -0,0 +1,10 @@
+import UIKit
+
+final class MainTableViewCell: UITableViewCell {
+
+    // MARK: IBOutlets
+    
+    @IBOutlet weak var titleLabel: UILabel!
+    
+    
+}
--- MovieWorld/MovieWorld/PresentationLayer/Main/MainViewController.swift
+++ MovieWorld/MovieWorld/PresentationLayer/Main/MainViewController.swift
@@ -2,10 +2,42 @@ import UIKit
 
 final class MainViewController: BaseViewController {
     
+    // MARK: IBOutlets
+    
+    @IBOutlet weak var mainTableView: UITableView!
+    
+    // MARK: Properties
+    
+    private var movies = ["Ring of Rows","Spider Man","Venom","Arrow"]
+    
     // MARK: Lifecycle functions
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .red
+        mainTableView.register(UINib(nibName: "MainTableViewCell", bundle: nil), forCellReuseIdentifier: "MainTableViewCell")
+    }
+}
+
+// MARK: - UITableViewDelegate
+extension MainViewController: UITableViewDelegate {
+    
+}
+
+// MARK: - UITableViewDataSource
+extension MainViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView,
+                   numberOfRowsInSection section: Int) -> Int {
+        return movies.count
+    }
+    
+    func tableView(_ tableView: UITableView,
+                   cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "MainTableViewCell", for: indexPath) as? MainTableViewCell else {
+            return UITableViewCell()
+        }
+        
+        cell.titleLabel.text = movies[indexPath.row]
+        
+        return cell
     }
 }
